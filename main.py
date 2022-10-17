1a93a087c85dd6203173d35141b2866969a370e7
Your Name <you@example.com>
2022-10-16 20:42:45 -0700

    code

--- main.py
+++ main.py
@@ -11,7 +11,15 @@ numbers = [    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 rnum = len(numbers)
 cnum = len(numbers[0])
-
+crosscount=0;
+for i in range(rnum):
+    for j in range(cnum):
+        if i>0 and j>0 and i<rnum-1 and j<cnum-1:
+            if numbers[i][j]==1 and numbers[i-1][j]==1 and numbers[i+1][j]==1 and numbers[i][j+1]==1 and numbers[i][j-1]==1:
+                crosscount+=1;
+                if numbers[i][j]==1 and numbers[i][j+1]==1 and numbers[i][j-1]==1 and numbers[i+1][j]==1 and numbers[i+1][j-1]==1 and numbers[i+1][j+1]==1:
+                    crosscount-=1;
+    
+print(crosscount)
 # ******************************
 # Make your Code
-# ******************************
